ee3d8ace1be870bfa6f7f66fcd3dedf316336c61
thomaslantern <shikisha@hotmail.com>
2023-03-27 05:46:17 -0400

    added intro music

--- birthdayblast.asm
+++ birthdayblast.asm
@@ -32,6 +32,153 @@ randomnum1 equ $0C
 randomnum2 equ $0D
 walkanimation equ $FF	
 
+
+A1 equ $00
+As1 equ $01
+Bb1 equ $01
+B1 equ $02
+
+C2 equ $03
+Cs2 equ $04
+Db2 equ $04
+D2 equ $05
+Ds2 equ $06
+Eb2 equ $06
+E2 equ $07
+F2 equ $08
+Fs2 equ $09
+Gb2 equ $09
+G2 equ $0A
+Gs2 equ $0B
+Ab2 equ $0B
+A2 equ $0C
+As2 equ $0D
+Bb2 equ $0D
+B2 equ $0E
+
+C3 equ $0F
+Cs3 equ $10
+Db3 equ $10
+D3 equ $11
+Ds3 equ $12
+Eb3 equ $12
+E3 equ $13
+F3 equ $14
+Fs3 equ $15
+Gb3 equ $15
+G3 equ $16
+Gs3 equ $17
+Ab3 equ $17
+A3 equ $18
+As3 equ $19
+Bb3 equ $19
+B3 equ $1A
+
+C4 equ $1B
+Cs4 equ $1C
+Db4 equ $1C
+D4 equ $1D
+Ds4 equ $1E
+Eb4 equ $1E
+E4 equ $1F
+F4 equ $20
+Fs4 equ $21
+Gb4 equ $21
+G4 equ $22
+Gs4 equ $23
+Ab4 equ $23
+A4 equ $24
+As4 equ $25
+Bb4 equ $25
+B4 equ $26
+
+C5 equ $27
+Cs5 equ $28
+Db5 equ $28
+D5 equ $29
+Ds5 equ $2A
+Eb5 equ $2A
+E5 equ $2B
+F5 equ $2C
+Fs5 equ $2D
+Gb5 equ $2D
+G5 equ $2E
+Gs5 equ $2F
+Ab5 equ $2F
+A5 equ $30
+As5 equ $31
+Bb5 equ $31
+B5 equ $32
+
+C6 equ $33
+Cs6 equ $34
+Db6 equ $34
+D6 equ $35
+Ds6 equ $36
+Eb6 equ $36
+E6 equ $37
+F6 equ $38
+Fs6 equ $39
+Gb6 equ $39
+G6 equ $3A
+Gs6 equ $3B
+Ab6 equ $3B
+A6 equ $3C
+As6 equ $3D
+Bb6 equ $3D
+B6 equ $3E
+
+C7 equ $3F
+Cs7 equ $40
+Db7 equ $40
+D7 equ $41
+Ds7 equ $42
+Eb7 equ $42
+E7 equ $43
+F7 equ $44
+Fs7 equ $45
+Gb7 equ $45
+G7 equ $46
+Gs7 equ $47
+Ab7 equ $47
+A7 equ $48
+As7 equ $49
+Bb7 equ $49
+B7 equ $4A
+
+C8 equ $4B
+Cs8 equ $4C
+Db8 equ $4C
+D8 equ $4D
+Ds8 equ $4E
+Eb8 equ $4E
+E8 equ $4F
+F8 equ $50
+Fs8 equ $51
+Gb8 equ $51
+G8 equ $52
+Gs8 equ $53
+Ab8 equ $53
+A8 equ $54
+As8 equ $55
+Bb8 equ $55
+B8 equ $56
+
+C9 equ $57
+Cs9 equ $58
+Db9 equ $58
+D9 equ $59
+Ds9 equ $5A
+Eb9 equ $5A
+E9 equ $5B
+F9 equ $5C
+Fs9 equ $5D
+
+sqlen equ $7A	; counter for square note duration
+ctnt equ $7C  	; current note
+ntnum equ $7D	; number of current note
+
+
 nmihandler:
 	pha
 	php
@@ -41,6 +188,16 @@ nmihandler:
 
 	lda #$02
 	sta $4014
+
+	ldx ctnt
+	cpx #0
+	beq skipnote
+	
+playnote:
+	jsr soundframe
+
+skipnote:
+
 
 	jsr readcontroller
 	
@@ -365,6 +522,36 @@ scoresprites:
 	sta $0207		; Store starting x-coord
 
 
+musicsetup:
+	; Initialization
+	lda #$01
+	sta $4015	; Turn on instruments
+	lda #%11111111
+	sta $4000	; Configure square 1
+	lda #$00
+	sta $4001	; Turn off sweeping on square 1
+
+	
+	; Load initial values
+	; Then turn the screen on so music can play
+	lda birthday_notes
+	sta ctnt
+	asl		; Double it since we're dealing with words	
+	tax		; Put the value in x-register
+
+	lda notes,x 	; Lower half of note
+	sta $4002
+	lda notes+1,x 	; Higher half of note
+	sta $4003
+
+	; Length of first note
+	lda birthday_length	
+	sta sqlen
+	
+	lda #0
+	sta ntnum	; Number of note starts at zero
+	
+
 	; turn screen on
 	lda #%00011110
 	sta $2001
@@ -372,10 +559,105 @@ scoresprites:
 	sta $2000
 
 
-
-
 forever:
 	jmp forever
+
+
+soundframe:
+
+	; This subroutine is only loaded via vblank
+	; and only if there are still notes to play
+
+	; Check first if a new note is needed
+	; If not, we simply decrease counter
+	; and move on
+
+	lda sqlen
+	bne decreasecount
+	
+	; sqlen is zero, so we
+	; need a new note
+	; Increase note counter
+	ldx ntnum	
+	inx
+	stx ntnum
+	
+
+	lda birthday_notes,x
+	sta ctnt
+	
+	; Make sure newest note is a note
+	cmp #0
+	bne newnote
+	
+silence:
+
+	; Note is "zero", so stop music
+	lda #0
+	sta $4015
+	rts
+
+newnote:
+	
+
+	; Load up a new note		
+	ldx ntnum
+	lda birthday_length,x
+	sta sqlen	; Length of new note
+
+	ldx ctnt
+
+	txa
+	asl		; Double value since using words
+	tax		; Put back in x-register
+
+	lda notes,x
+	sta $4002
+	lda notes+1,x
+	sta $4003
+
+decreasecount:
+	ldx sqlen
+	dex
+	stx sqlen
+
+	rts 		; exit subroutine
+
+
+; Table of different notes and their values
+notes:	
+	dw $07F1, $0780, $0713 						; A1 to B1 ($00-$02)
+	dw $06AD, $064D, $05F3, $059D, $054D, $0500	; C2 to F2 ($03-$08)
+	dw $04B8, $0475, $0435, $03F8, $03BF, $0389 ; F#2 to B2 ($09-$0E)
+	dw $0356, $0326, $02F9, $02CE, $02A6, $027F	; C3 to F3 ($0F-$15)
+	dw $025C, $023A, $021A, $01FB, $01DF, $01C4 ; F#3 to B3 ($16-$1A)
+	dw $01AB, $0193, $017C, $0167, $0151, $013F	; C4 to F4 ($1B-$20)
+	dw $012D, $011C, $010C, $00FD, $00EF, $00E2 ; F#4 to B4 ($20-$26)
+	dw $00D2, $00C9, $00BD, $00B3, $00A9, $009F	; C5 to F5 ($27-$2C)
+	dw $0096, $008E, $0086, $007E, $0077, $0070 ; F#5 to B5 ($2D-$32)
+	dw $006A, $0064, $005E, $0059, $0054, $004F	; C6 to F6 ($33-$38)
+	dw $004B, $0046, $0042, $003F, $003B, $0038 ; F#6 to B6 ($39-$3E)
+	dw $0034, $0031, $002F, $002C, $0029, $0027	; C7 to F7 ($3F-$45)
+	dw $0025, $0023, $0021, $001F, $001D, $001B ; F#7 to B7 ($46-$4A)
+	dw $001A, $0018, $0017, $0015, $0014, $0013	; C8 to F8 ($4B-$50)
+	dw $0012, $0011, $0010, $000F, $000E, $000D ; F#8 to B8 ($51-$56)
+	dw $000C, $000C, $000B, $000A, $000A, $0009, $0008 ; C9 to F#9 ($57-$5D)
+
+; Notes to the song ("Happy Birthday")
+birthday_notes:
+	db G3, G3, A3, G3, C4, B3
+	db G3, G3, A3, G3, D4, C4
+	db G3, G3, G4, E4, C4, B3, A3
+	db F4, F4, E4, C4, D4, C4
+	db 0 ; Zero indicates song is over
+
+birthday_length:
+	
+	; 240 BPM -> 4 times a second -> 15 counts per quarter note
+	db 30, 15, 45, 45, 45, 90
+	db 30, 15, 45, 45, 45, 90
+	db 30, 15, 45, 45, 45, 45, 90
+	db 30, 15, 45, 45, 45, 105
 
 
 ; Walking Animation
